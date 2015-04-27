c7035f2dc37a6bd42f525d040af96756aaaa78bb
Alexander Solovov <solovov@gmail.com>
2015-04-27 22:15:29 +0600

    Unused EJBs have been removed and database cleaned from the unused settings.

---
+++ transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/referencedata/ClaimStatusTO.java
@@ -0,0 +1,9 @@
+package org.sola.services.boundary.transferobjects.referencedata;
+
+import org.sola.services.common.contracts.AbstractCodeTO;
+
+public class ClaimStatusTO extends AbstractCodeTO {
+    public ClaimStatusTO(){
+        super();
+    }
+}
