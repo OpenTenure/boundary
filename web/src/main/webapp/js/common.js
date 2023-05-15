a8ac643aaa404495a78e700fc8101e6752a3dea7
Alexander Solovov <a_solovov@yahoo.com>
2023-05-15 18:55:04 +0600

    Added function to correct protocol type if the system deployed behind proxy redirector

--- web/src/main/webapp/js/common.js
+++ web/src/main/webapp/js/common.js
@@ -1,3 +1,14 @@
 $(function () {
     $("[rel='tooltip']").tooltip();
 });
+
+fixAppProto = function (appUrl) {
+    var proto = window.location.href;
+    proto = proto.substring(0, proto.indexOf(":"));
+
+    if(!appUrl.startsWith(proto)) {
+        appUrl = proto + appUrl.substring(appUrl.indexOf(":"));
+    }
+
+    return appUrl;
+};
